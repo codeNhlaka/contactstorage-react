c265095433c1e9fb79b65483255598310bfab14a
Nhlakanipho <nhlaka.devv@gmail.com>
2021-07-19 15:19:40 +0200

    changes to the config file

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 const config = {
     apiGateway: {
         REGION: "us-east-2",
-        URL: "https://0bbyo859l9.execute-api.us-east-2.amazonaws.com/dev"
+        URL: "<URL>"
     },
     cognito: {
         REGION: "us-east-2",
-        USER_POOL_ID: "us-east-2_r2adG5BCB",
-        APP_CLIENT_ID: "36ksfkce6qnvr481g2bt742p83"
+        USER_POOL_ID: "<UserPoolID>",
+        APP_CLIENT_ID: "<ClientID>"
     }
 }
 
